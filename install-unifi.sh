cbce37dedaf994af8779178151233b81f612517c
htunzoo <76261147+htunzoo@users.noreply.github.com>
2020-12-19 09:57:30 -0800

    Update install-unifi.sh

--- install-unifi.sh
+++ install-unifi.sh
@@ -7,7 +7,7 @@
 UNIFI_SOFTWARE_URL="http://dl.ubnt.com/unifi/6.0.41/UniFi.unix.zip"
 
 # The rc script associated with this branch or fork:
-RC_SCRIPT_URL="https://raw.githubusercontent.com/gozoinks/unifi-pfsense/master/rc.d/unifi.sh"
+RC_SCRIPT_URL="https://github.com/htunzoo/unifi/blob/main/unifi.sh"
 
 
 # If pkg-ng is not yet installed, bootstrap it:
